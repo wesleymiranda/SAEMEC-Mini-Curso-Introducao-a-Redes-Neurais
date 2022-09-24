e32056afdf83e6472d8a92aa6189413b0bdfdf44
wesleymirada <wymirandac@gmail.com>
2022-09-24 09:29:07 -0400

    Criadas as funções de entrada e saída de dados

--- NN.m
+++ NN.m
@@ -1,18 +1,28 @@
 %UNIVERSIDADE FEDERAL DE RONDONÓPOLIS
 %CURSO DE ENGENHARIA MECÂNICA
-%
-clear ; close all; clc
+
+%===============================================================================
+%                               CABEÇALHO
+%===============================================================================
+
+clear ; close all; clc;
+source("extras.m");
+
 
 %===============================================================================
 %                             INICIALIZANDO A NN
 %===============================================================================
-tam_camada_entrada = 20;
+tam_camada_entrada = 50;
 tam_camada_oculta = 20;
-tam_camada_saida = 10;
+tam_camada_saida = 9;
 lambda = 1;
 
 W1_inicial = inicializarPesos(tam_camada_entrada, tam_camada_oculta);
 W2_inicial = inicializarPesos(tam_camada_oculta, tam_camada_saida);
 
-
+%===============================================================================
+%                        EXTRAINDO SAIDAS E ENTRADAS
+%===============================================================================
+tabela = criarTabela(tam_camada_saida);
+entradas = extrairEntradas();
 
--- extras.m
+++ extras.m
@@ -1,24 +1,99 @@
-extraspkg load image
+%UNIVERSIDADE FEDERAL DE RONDONÓPOLIS
+%CURSO DE ENGENHARIA MECÂNICA
+
+%===============================================================================
+%                              CABEÇALHO
+%===============================================================================
+
+1; %Usado para o Octave identificar que se trata de um script file,
+   %e não confundir com um function file
+
+%===============================================================================
+%                         PREPARANDO A TABELA VERDADE
+%===============================================================================
+
+function [arquivos, pasta, formato] = lendoArquivos()
+  pasta = '.\img\CODES 2D\';
+  formato = '.png';
+  arquivos = glob(strcat(pasta, "*", formato));
+endfunction
+
+%===============================================================================
+%                         PREPARANDO A TABELA VERDADE
+%===============================================================================
+
+function tabela = criarTabela(n_saidas)
+
+  [arquivos_dir, pasta, formato] = lendoArquivos();
+  arquivos_format = strrep(arquivos_dir, pasta, "");
+  arquivos_nome = strrep(arquivos_format, formato, "");
+
+  m = size(arquivos_nome, 1);
+
+  tabela = zeros(m, n_saidas);
+
+  for i = 1:m
+   tabela(i, str2num(arquivos_nome{i, 1})) = 1;
+  endfor
+
+endfunction
+
+%===============================================================================
+%                         EXTRAIR ENTRADAS
+%===============================================================================
+
+function entradas = extrairEntradas()
+
+  arquivos_dir = lendoArquivos();
+  m = size(arquivos_dir, 1);
+
+  entradas = []
+
+  for i = 1:m
+    img = imread(arquivos_dir{i, 1});
+    img_bn = threshold(img)
+    entradas = [img_bn, entradas];
+  endfor
+
+endfunction
+
+%===============================================================================
+%                         PREPARANDO A TABELA VERDADE
+%===============================================================================
+
+function pesos = inicializarPesos(tam_anterior, tam_prox)
+  pesos = rand(tam_prox, tam_anterior);          #pesos(w) randomicos [linhas]=saidas [colunas]=entradas
+  pesos = [ones(tam_prox, 1), pesos];            #Bias
+ endfunction
+
+%===============================================================================
+%                                  THRESHOLD
+%===============================================================================
+
+function img_bn = threshold(img_rgb, thresh)
+
+  img_bn = rgb2gray(img_rgb);
+  m = size(img_bn);
+
+  for i = 1:m(1)
+    for j = 1:m(2)
+      if img_bn(i, j) >= thresh
+        img_bn(i, j) = 1;
+      else
+        img_bn(i, j) = 0;
+      endif
+   endfor
+  endfor
+ endfunction
 
 
-img = imread("carro.jpg");
-img = imresize(img, [100, 100]);
-image(img);
-imwrite(img, "img.jpg");
+%===============================================================================
+%                               FUNÇÃO DE CUSTO
+%===============================================================================
+ function J = funcaoCusto(W1, W2, entrada)
+  m = size(W1, 1)                   #Camada entrada: linhas x 1
 
-%=============================================
-%TABELA
 
-y_new = zeros(num_labels, m); % 10*5000
-for i=1:m,
-  y_new(y(i),i)=1;
-end
+endfunction
 
-%============================================
-%INICIALIZAR PESOS ALEATORIAMENTE
-function W = inicializarPesos(tam_entrada, tam_saida)
-
-  W = zeros(tam_saida, tam_entrada + 1);
-  W = rand(tam_saida, tam_entrada +1);
- end
 
