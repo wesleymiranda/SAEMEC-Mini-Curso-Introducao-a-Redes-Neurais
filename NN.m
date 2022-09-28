ca76f038607054ba35e13ef453cb9f1d7b33abbf
wesleymirada <wymirandac@gmail.com>
2022-09-28 15:20:54 -0400

    Implementacao da rede multilayer perceptron

--- NN.m
+++ NN.m
@@ -1,5 +1,8 @@
-%UNIVERSIDADE FEDERAL DE RONDONÓPOLIS
-%CURSO DE ENGENHARIA MECÂNICA
+%                   UNIVERSIDADE FEDERAL DE RONDONÓPOLIS
+%                       CURSO DE ENGENHARIA MECÂNICA
+%                 SEMANA ACADÊMICA DA ENGENHARIA MECÂNICA
+%                                SAEMEC 2022
+
 
 %===============================================================================
 %                               CABEÇALHO
@@ -10,9 +13,12 @@ source("extras.m");
 
 
 %===============================================================================
+
+
+
 %                             INICIALIZANDO A NN
 %===============================================================================
-tam_camada_entrada = 50;
+tam_camada_entrada = 20*20;
 tam_camada_oculta = 20;
 tam_camada_saida = 9;
 lambda = 1;
@@ -20,9 +26,23 @@ lambda = 1;
 W1_inicial = inicializarPesos(tam_camada_entrada, tam_camada_oculta);
 W2_inicial = inicializarPesos(tam_camada_oculta, tam_camada_saida);
 
+pesos_iniciais = [reshape(W1_inicial, numel(W1_inicial), 1);...
+                 reshape(W2_inicial, numel(W2_inicial), 1)];
+
 %===============================================================================
 %                        EXTRAINDO SAIDAS E ENTRADAS
 %===============================================================================
 tabela = criarTabela(tam_camada_saida);
 entradas = extrairEntradas();
 
+funcCusto = @(p) funcaoCusto(p, tam_camada_entrada, tam_camada_oculta, entradas, tabela, lambda);
+[pesosw] = fminunc(funcCusto, pesos_iniciais, optimset('MaxIter', 15));
+
+imgn = imread('.\img\CODES 2D\2.png');
+img_bn = threshold(imgn, 0.3);
+img_linha = reshape(img_bn, numel(img_bn), 1);
+img_linha = im2double(img_linha);
+
+
+#predini = predicao(img_linha, pesos_iniciais, tam_camada_entrada, tam_camada_oculta, tam_camada_saida);
+pred = predicao( X, pesosw, tam_camada_entrada, tam_camada_oculta, tam_camada_saida, tabela);
--- extras.m
+++ extras.m
@@ -1,5 +1,7 @@
-%UNIVERSIDADE FEDERAL DE RONDONÓPOLIS
-%CURSO DE ENGENHARIA MECÂNICA
+%                   UNIVERSIDADE FEDERAL DE RONDONÓPOLIS
+%                       CURSO DE ENGENHARIA MECÂNICA
+%                 SEMANA ACADÊMICA DA ENGENHARIA MECÂNICA
+%                                SAEMEC 2022
 
 %===============================================================================
 %                              CABEÇALHO
@@ -9,7 +11,7 @@
    %e não confundir com um function file
 
 %===============================================================================
-%                         PREPARANDO A TABELA VERDADE
+%                               ABRIR IMAGENS
 %===============================================================================
 
 function [arquivos, pasta, formato] = lendoArquivos()
@@ -47,23 +49,27 @@ function entradas = extrairEntradas()
   arquivos_dir = lendoArquivos();
   m = size(arquivos_dir, 1);
 
-  entradas = []
+  entradas = [];
 
   for i = 1:m
     img = imread(arquivos_dir{i, 1});
-    img_bn = threshold(img)
-    entradas = [img_bn, entradas];
+    img_bn = threshold(img, 0.3);
+    img_linha = reshape(img_bn, numel(img_bn), 1);
+    entradas = [entradas, img_linha];
   endfor
+
+
 
 endfunction
 
 %===============================================================================
-%                         PREPARANDO A TABELA VERDADE
+%                              INICIALIZAR PESOS
 %===============================================================================
 
 function pesos = inicializarPesos(tam_anterior, tam_prox)
-  pesos = rand(tam_prox, tam_anterior);          #pesos(w) randomicos [linhas]=saidas [colunas]=entradas
-  pesos = [ones(tam_prox, 1), pesos];            #Bias
+  pesos = rand(tam_prox, tam_anterior+1);          #pesos(w) randomicos [linhas]=saidas [colunas]=entradas
+  pesos = pesos - 0.5
+
  endfunction
 
 %===============================================================================
@@ -72,7 +78,9 @@ function pesos = inicializarPesos(tam_anterior, tam_prox)
 
 function img_bn = threshold(img_rgb, thresh)
 
+
   img_bn = rgb2gray(img_rgb);
+  img_bn = im2double(img_bn);
   m = size(img_bn);
 
   for i = 1:m(1)
@@ -85,15 +93,62 @@ function img_bn = threshold(img_rgb, thresh)
    endfor
   endfor
  endfunction
+%===============================================================================
+%                                   SIGMOID
+%===============================================================================
 
+function g = sigmoid(z)
+  g = 1 ./ (1 + e.^-z);
+endfunction
+
+%===============================================================================
+%                              SIGMOID GRADIENT
+%===============================================================================
+
+function g = sigmoidGradient(z)
+  g = sigmoid(z).*(1-sigmoid(z));
+endfunction
 
 %===============================================================================
 %                               FUNÇÃO DE CUSTO
 %===============================================================================
- function J = funcaoCusto(W1, W2, entrada)
-  m = size(W1, 1)                   #Camada entrada: linhas x 1
+ function [J] = funcaoCusto(pesos,tam_cam_input, tam_cam_hiden, X, y, lambda)
 
+  W1 = reshape(pesos(1:tam_cam_hiden*(tam_cam_input+1)), tam_cam_hiden, tam_cam_input+1);
+  W2 = reshape(pesos(tam_cam_hiden*(tam_cam_input+1)+1:end), length(y), tam_cam_hiden+1);
+
+  m = size(X, 2);                   #Camada entrada: linhas x 1
+  J = 0;
+
+  X = [ones(1, columns(X)); X];
+  a1 = W1*X;
+  z1 = sigmoid(a1);
+
+  a2 = [ones(1, columns(a1)); z1];
+  z2 = W2*a2;
+  h = sigmoid(z2);
+
+  J = (1/m) * sum(sum((-y).*log(h)-(1-y).*log(1-h)))
 
 endfunction
+%===============================================================================
+%                               FUNÇÃO DE CUSTO
+%===============================================================================
+function h = predicao(X, W, tam_cam_input, tam_cam_hiden, tam_cam_out, y)
 
+  W1 = reshape(W(1:tam_cam_hiden*(tam_cam_input+1)), tam_cam_hiden, tam_cam_input+1);
+  W2 = reshape(W(tam_cam_hiden*(tam_cam_input+1)+1:end), tam_cam_out, tam_cam_hiden+1);
+  m = size(X, 2);
+
+  X = [ones(1, columns(X)); X]
+  a1 = W1*X
+  z1 = sigmoid(a1)
+
+  a2 = [ones(1, columns(a1)); z1]
+  z2 = W2*a2
+  h = sigmoid(z2)
+
+  J = (1/m) * sum(sum((-y).*log(h)-(1-y).*log(1-h)));
+
+  endfunction
 
